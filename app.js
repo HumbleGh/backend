14efce00f5e0ecea87dec016ed2b5a3334d20eb5
Humble-Preacher <61693488+Humble-Preacher@users.noreply.github.com>
2022-07-04 15:37:58 +0000

    creating schemas

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 require("dotenv/config");
 
-// const productsRouter = require('./routes/products');
+app.us(cors());
+app.options('*', cors())
+
+
 
 // middleware
 app.use(bodyParser.json());
