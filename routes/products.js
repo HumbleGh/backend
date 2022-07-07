0007f5e7729617954d450b93ad0f2245e6616ead
Humble-Preacher <61693488+Humble-Preacher@users.noreply.github.com>
2022-07-07 23:12:28 +0000

    changes made to products

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,6 @@
 const {Product} = require('../models/product');
 const express = require('express');
+const { Category } = require('../models/category');
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
@@ -11,9 +12,22 @@ router.get(`/`, async (req, res) => {
         res.send(productList);
 })
 
+
+router.get(`/`, async (req, res) => {
+    const productList = await Product.find();
+
+    if (!productList){
+        res.status(500).json({success: false});
+    }
+        res.send(productList);
+})
+
 // creating a product from the frontend 
-router.post(`/`, (req, res) => {
-   const product = new Product({
+router.post(`/`, async (req, res) => {
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalid Category!')
+
+    const product = new Product({
     name: req.body.name,
     description: req.body.description,
     richDescription: req.body.richDescription,
@@ -27,14 +41,12 @@ router.post(`/`, (req, res) => {
     isFeatured: req.body.isFeatured,
    })
 
-   product.save().then((createdProduct => {
-    res.status(201).json(createdProduct)
-   })).catch((err) => {
-    res.status(500).json({
-        error: err,
-        success: false
-    })
-   })
+   product = await product.save();
+
+   if (!product)
+   return res.status(500).send('The product cannot be created')
+
+   res.send(product);
 })
 
 module.exports = router;
