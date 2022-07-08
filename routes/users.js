0eba2003803340795b56eca283cbe9daa5c6f714
Humble-Preacher <61693488+Humble-Preacher@users.noreply.github.com>
2022-07-08 13:11:19 +0000

    creating Users schemas

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,25 @@ router.get(`/`, async (req, res) => {
         res.send(userList);
 })
 
+router.post('/', async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        color: req.body.color,
+        passwordHash: req.body.passwordHash,
+        phone:req.body.phone,
+        isAdmin: req.body.isAdmin,
+        appartment: req.body.appartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    }) 
+    user = await user.save();
+
+    if(!user)
+    return res.status(400).send('The user cannot be created!')
+
+    res.send(user);
+})
+
 module.exports = router;
