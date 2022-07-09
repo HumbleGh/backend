a1ebec02b13c8b49f90a6aae5c812c12cae71175
Humble-Preacher <61693488+Humble-Preacher@users.noreply.github.com>
2022-07-09 14:33:14 +0000

    minor changes

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,12 @@ const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.get(`/`, async (req, res) => {
     const userList = await User.find().select('-passwordHarsh');
 
-    if (!userList) {
+    if(!userList) {
         res.status(500).json({success: false});
     }
     res.send(userList);
@@ -19,39 +20,8 @@ router.get('/:id',async(req, res) =>{
         res.status(500).json({message: 'The user with this ID was not found!'})
     }
     res.status(200).send(user);
-} )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-router.post('/', async (req, res) => {
+})
+  router.post('/', async (req, res) => {
     let user = new User({
         name: req.body.name,
         email: req.body.email,
@@ -82,7 +52,13 @@ router.post('/login', async (req, res) => {
         return res.status(400).send('The user is not found');
     }
 
-    return res.status(200).send(user);
+    if(user && bycript.compareSync(req.body.passwordHash)){
+        res.status(200).send('User Authentication')
+    }else {
+        return res.status(400).send('pasword is wrong');
+    }
+
+
 })
 
 
