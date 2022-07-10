4e27a5a92027c14b872b22b687942554fb1dcf29
Humble-Preacher <61693488+Humble-Preacher@users.noreply.github.com>
2022-07-10 14:57:06 +0000

    Updating Users

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,7 @@ router.get(`/`, async (req, res) => {
 router.get('/:id',async(req, res) =>{
     const user = await User.findById(req.params.id).select('-passwordHarsh');
 
-    if (!user) {
+    if(!user) {
         res.status(500).json({message: 'The user with this ID was not found!'})
     }
     res.status(200).send(user);
@@ -33,11 +33,9 @@ router.get('/:id',async(req, res) =>{
         zip: req.body.zip,
         city: req.body.city,
         country: req.body.country,
-    },
-    {new: true}
-
-    ) 
-
+    })
+    user = await user.save();
+   
 
     if(!user)
     return res.status(400).send('The user cannot be created!')
@@ -45,17 +43,57 @@ router.get('/:id',async(req, res) =>{
     res.send(user);
 })
 
+router.put('/:id', async (req, res) => {
+
+    const userExist = await User.findById(req.params.id);
+    let newPassword
+    if(req.body.password) {
+        newPassword = bcrypt.hashSync(req.body.password, 10)
+    } else {
+        newPassword = userExist.passwordHash;
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: newPassword,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        appartment: req.body.appartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+        },
+        {new: true}
+    )
+
+    if (!user)
+    return res.status(400).send('The user cannot be created!')
+
+    res.send(user);
+})
+
 router.post('/login', async (req, res) => {
-    const user = await user.findOne({email: req.body.email})
+    const user = await User.findOne({email: req.body.email})
 
     if(!user) {
         return res.status(400).send('The user is not found');
     }
 
-    if(user && bycript.compareSync(req.body.passwordHash)){
-        res.status(200).send('User Authentication')
-    }else {
-        return res.status(400).send('pasword is wrong');
+    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+        const token = jwt.sign(
+            {
+                userId: user.id
+            },
+            'secret'
+        )
+
+        res.status(200).send({user: user.email, token: token})
+    } else {
+        res.status(400).send('password is wrong!');
     }
 
 
